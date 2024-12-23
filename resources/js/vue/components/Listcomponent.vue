fc7694e30a087ba50b124f32413c153f13478689
diego ortega <diego-chido2011@hotmail.com>
2024-12-23 17:23:37 -0600

    Paginacion

--- resources/js/vue/components/Listcomponent.vue
+++ resources/js/vue/components/Listcomponent.vue
@@ -13,8 +13,9 @@
             Metemos los datos obtenidos de la RESP APi a esta tabla 
             En "data" indicamos los datos que estamos trabajando y le especificamos dos puntos
             para que evalue la condicion en JS que le colocamos (Le pasamos la pripeidad lista que tiene los datos)
+            ponemos "posts.data" para acceder a los datos de la API ya que por defecto obtenemos los datos con todas las propieades del JSON
          -->
-         <o-table :data="posts">
+         <o-table :data="posts.data" :loading="isLoading">
             <!-- "filed" tiene que tener el mismo nombre del JSON que obtenemos de respuesta 
                 "label" es el como se va a mostrar en la pagina
                 "v-slot" es para crear una variable local en este ambito que la usaremos para imprimr la data
@@ -33,32 +34,54 @@
             </o-table-column>
          </o-table>
 
+         <!-- 
+            Componente de Paginacion 
+            "v-if" -> Es la condicion para cuando solamente lo vamos a mostrar (Si al inicio no tiene datos dara error
+                        asi que le ponemos una condicion AND para que pueda verificar la propiedad Lenght donde al inicio
+                        verifica si se encuentra definido el arreglo)
+            "@change" -> Llamamos la funcion que creamos para actualizar la propiedad de paginacion
+            "v-model" -> Es la forma que tenemos para mapear o hacer match entre una propieadad definida en data()
+                         con un campo que puede ser un input (o cualquier otro del formulario) y asi obtener su valor
+            Definimos los rangos que vamos a mostrar antes o despues 
+            "size" -> Le definimos el nombre de la etiqueta HTML
+            Le decimos que queremos un diseno simple
+            Lo queremos redondeado
+            Le decimos que por paginas tome lo que ya tenemos en las propiedades
+          -->
+          <o-pagination
+            v-if="posts.data && posts.data.length > 0"
+            @change="updatePage"
+            :total="posts.total"
+            v-model:current="currentPage"
+            :range-before="2"
+            :range-after="2"
+            size="small"
+            :simple="false"
+            :rounded="true"
+            :per-page="posts.per_page"
+          >
+
+          </o-pagination>
+
     </div>
 </template>
 <script>
+
     export default {
         // Aqui colocamos las propiedades que va a tener el componente
         data() {
             return {
                 posts: [],// Aqui almacenaremos los datos que vamos a obtener
                 isLoading: true,
+                // Esta popiedad es para la paginacion
+                currentPage: 1,
             }
         },
         // Este metodo forma parte del ciclo de vida de la APP en vue
         // Esta sera una funcion que se ejecuta cuando se monta el componente (Que lo consume el usuario)
         mounted(){
             // Aqui dentro inicializamos los datos del componente (Peticion al API Rest)
-            // Consumismo la ruta que configuramos en nuestro API de Laravel
-            // como son peticiones asyncronas tenemos que usar el Then para manejarlas por los Promesas
-            // Dentro del then recibimos la respuesta y despues definimos lo que queremos hacer con esa respuesta
-            this.$axios.get('/api/post').then((res) => {
-                // Vemos los datos que tenemos
-                //console.log(res.data.data);
-
-                // Almacenamos la data en la pripiedad del arreglo
-                this.posts = res.data.data;
-                this.isLoading = false;
-            });
+            this.listPage();
         },
         // Estos son los metodos que creamos nosotros
         methods:{
@@ -66,6 +89,30 @@
                 // Para usar las propiedades globales lo llamamos con this.
                 //console.log(this.$axios);
                 alert('Funciono Oruga')
+            },
+            updatePage(){
+                console.log(this.currentPage);
+                setTimeout(()=>{
+                    this.listPage();
+                },100);
+            }, 
+            listPage(){
+                this.isLoading = true;
+                // Consumismo la ruta que configuramos en nuestro API de Laravel
+                // como son peticiones asyncronas tenemos que usar el Then para manejarlas por los Promesas
+                // Dentro del then recibimos la respuesta y despues definimos lo que queremos hacer con esa respuesta
+                //
+                // Pasamos parametros (?page=VALOR), el valor es la propiedad de paginacion que creamos
+                this.$axios.get('/api/post?page='+this.currentPage).then((res) => {
+                    // Vemos los datos que tenemos
+                    //console.log(res.data.data);
+
+                    // Almacenamos la data en la propiedad del arreglo
+                    // Con: "res.data.data" obtenemos el dato directamente de la API
+                    // Con: "res.data" Obtenemos la estrctura completa de AXIOS y asi tener acceso a las propieades de paginacion que ya vienen en laravel
+                    this.posts = res.data;
+                    this.isLoading = false;
+                });
             }
         }
     }
