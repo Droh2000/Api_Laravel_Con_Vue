0a5f42ae4551d2e1bab2c2c864cf93135f267ab4
diego ortega <diego-chido2011@hotmail.com>
2025-01-12 17:00:22 -0600

    Authenticacion en base a Tokens Config Inicial

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use laravel\Sanctum\HashApiTokens; // Para iniciar sesion por Tokens
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HashApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
