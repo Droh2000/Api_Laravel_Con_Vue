c51857c8dce4ce18a2aa2ef95476b30d10513b8e
diego ortega <diego-chido2011@hotmail.com>
2025-02-24 21:09:57 -0600

    Seccion 16: Gate y Politicas

---
+++ database/migrations/2025_02_25_025517_add_user_id_to_posts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // La columna va a ser de tipo foreignId dentro le especificamos el nombre de la columna y el tipo de relacion "cascade"
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // Aqui eliminamos la columna que creamos arriba
+            $table->dropColumn('user_id');
+        });
+    }
+};
