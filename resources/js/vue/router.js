c0ac4b81e25cdcb9711891e7e0b4276ab4aa14ca
gomezfarm <gomez.farm@outlook.com>
2025-02-26 13:46:38 -0600

    Proteger Rutas por autenticacion requerida

--- resources/js/vue/router.js
+++ resources/js/vue/router.js
@@ -6,6 +6,10 @@ import List from './components/ListComponent.vue';
 import Save from './components/SaveComponent.vue';
 import Login from './components/auth/LoginComponent.vue';
 
+// Para obtener la cookie
+import { useCookies } from 'vue3-cookies';
+const { cookies } = useCookies();
+
 // Definimos las rutas para cada componente
 const routes = [
     {
@@ -31,6 +35,20 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(), // El que dice Hash le agrega la # en la URL y este es el funcionamiento normal de la URL
     routes: routes,
+});
+
+// Agregar una proteccion adicional (Asi si el usuario esta en Login y quiere ir a otra ruta automaticamente la pagina lo mandara a Login, no podra moverse a otra ruta)
+router.beforeEach( async(to, from, next) => {
+    // Obtenemos la cookie con el objeto de autenticacion
+    const auth = cookies.get('auth');
+
+    // Aqui "login" es el nombre de la ruta que definimos en "main.js"
+    if(!auth && to.name !== 'login'){
+        // Redireccionamos a login
+        return next({name:'login'});
+    }
+
+    next();
 });
 
 export default router
